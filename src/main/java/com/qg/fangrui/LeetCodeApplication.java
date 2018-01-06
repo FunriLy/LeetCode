53b38ce80ce2c989c582d90ec26b0957485773df
FunriLy <fr479910770@qq.com>
2018-01-06 15:22:15 +0800

    init

--- src/main/java/com/qg/fangrui/LeetCodeApplication.java
+++ src/main/java/com/qg/fangrui/LeetCodeApplication.java
@@ -3,6 +3,12 @@ package com.qg.fangrui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot 主类
+ * @author FunriLy
+ * Created by FunriLy on 2018/1/6.
+ * From small beginnings comes great things.
+ */
 @SpringBootApplication
 public class LeetCodeApplication {
 
