f86930e809c2ba39e3b0e8945a4416fd58d37cf7
FunriLy <fr479910770@qq.com>
2018-02-02 14:27:34 +0800

    add structure TreeLinkNode

---
+++ src/main/java/com/qg/fangrui/structure/TreeLinkNode.java
@@ -0,0 +1,27 @@
+package com.qg.fangrui.structure;
+
+/**
+ * 数据结构 TreeLinkNode
+ * @author FunriLy
+ * Created by FunriLy on 2018/2/2.
+ * From small beginnings comes great things.
+ */
+public class TreeLinkNode {
+
+    private int val;
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+    public TreeLinkNode(int v){
+        val = v;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+}
